5feb8ace7407e5f3c25efb0a25505f706db21c58
stranothus <83613280+stranothus@users.noreply.github.com>
2021-05-30 15:03:29 -0500

    Update dateToObj.js

--- dateToObj.js
+++ dateToObj.js
@@ -2,11 +2,11 @@
 
 function dateToObj(d) {
     return {
-        year : d.slice(11, 15),
-        month : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"].indexOf(d.slice(4, 7)) + 1,
-        day : d.slice(8, 10),
-        hour : d.slice(16, 18),
-        min : d.slice(19, 21),
-        sec : d.slice(22, 24)
+        year : d.getFullYear(),
+        month : d.getMonth() + 1,
+        day : d.getDate(),
+        hour : d.getHours() + 1,
+        min : d.getMinutes() + 1,
+        sec : d.getSeconds() + 1
     }
 }
